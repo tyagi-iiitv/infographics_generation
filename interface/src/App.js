ff5ba19e7acbbd0f0474775ebb2b910f662905bd
Pushkar Patel <thepushkarp@gmail.com>
2020-10-04 15:44:53 +0530

    Remove duplicate App.js and reindent lines

--- interface/src/App.js
+++ interface/src/App.js
@@ -17,77 +17,88 @@ class App extends React.Component {
         this.setState({text: this.state.text, items: new_items})
     }
 
-  render() {
+    render() {
 
-    return (
-        <div className="App">
-            <header className="App-header">
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                <input
-                id="input_dragable_text"
-                onChange={this.handleChange}
-                value={this.state.text}
-                placeholder = "Press Enter to input"
-                />
-                </form>
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <div>
+                        <form onSubmit={this.handleSubmit}>
+                            <input
+                                id="input_dragable_text"
+                                onChange={this.handleChange}
+                                value={this.state.text}
+                                placeholder = "Press Enter to input"
+                            />
+                        </form>
+                    </div>
+                </header>
+                <div className="App-body">
+                    {this.state.items}
+                </div>
             </div>
-            </header>
-            <div className="App-body">
-            {this.state.items}
-            </div>
-        </div>
-    );
-  }
-
-  handleChange(e) {
-    this.setState({ text: e.target.value });
-  }
-
-  handleSubmit(e) {
-        //show the remove button
-        function show(e) {
-            e.target.style.opacity= 1;
+            );
         }
 
-        //hide the remove button
-        function hide(e) {
-            e.target.style.opacity= 0;
+        handleChange(e) {
+            this.setState({ text: e.target.value });
         }
-        //if nothing input, ignore it
-        e.preventDefault();
-        if (this.state.text.length === 0) {
-            return;
+
+        handleSubmit(e) {
+            //show the remove button
+            function show(e) {
+                e.target.style.opacity= 1;
+            }
+
+            //hide the remove button
+            function hide(e) {
+                e.target.style.opacity= 0;
+            }
+            //if nothing input, ignore it
+            e.preventDefault();
+            if (this.state.text.length === 0) {
+                return;
+            }
+            const id =new Date().toLocaleString();
+            var input_text = this.state.text;
+            var number_sign = 0;
+            var input_size = 1;
+            var text_weight = 'normal';
+            var input_length = input_text.length
+            if (input_text[0] === '#') {
+                number_sign++;
+            }
+            this.state.text = input_text.substring(number_sign, input_length);
+            if (number_sign !== 0) {
+                text_weight = 'bold'
+            }
+            input_size = input_size + 1*number_sign;
+            const newItem =
+            //must have unique key to identify the Draggable class
+            <Draggable key={id}>
+                <div className="draggable_content">
+                    <div
+                        onClick={() => this.handleRemove(id)}
+                        className="remove_button"
+                        onMouseEnter={show}
+                        onMouseLeave={hide}
+                    >
+                        remove
+                    </div>
+                    <div>
+                        <text
+                            style={{fontSize: input_size +'em', fontWeight: text_weight}}
+                        >
+                            {this.state.text}
+                        </text>
+                    </div>
+                </div>
+            </Draggable>;
+            this.setState(state => ({
+                items: state.items.concat(newItem),
+                text: ''
+            }));
         }
-        const id =new Date().toLocaleString();
-        var input_text = this.state.text;
-        var number_sign = 0;
-        var input_size = 1;
-        var text_weight = 'normal';
-        var input_length = input_text.length
-        if (input_text[0] == '#') {
-            number_sign++;
-        }
-        this.state.text = input_text.substring(number_sign, input_length);
-        if (number_sign !== 0) {
-            text_weight = 'bold'
-        }
-        var input_size = 1 + 1*number_sign;
-        const newItem =
-        //must have unique key to identify the Draggable class
-        <Draggable key={id}>
-            <div className="draggable_content">
-            <div onClick={() => this.handleRemove(id)} className="remove_button"
-            onMouseEnter={show}
-            onMouseLeave={hide}>remove</div>
-            <div><text style={{fontSize: input_size +'em', fontWeight: text_weight}}>{this.state.text}</text></div>
-            </div>
-        </Draggable>;
-        this.setState(state => ({
-            items: state.items.concat(newItem),
-            text: ''
-        }));
     }
-}
 
-export default App;
+    export default App;
