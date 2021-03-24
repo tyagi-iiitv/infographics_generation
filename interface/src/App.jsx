1184c9efcae058fd18c269faaedffe39096a02a9
tyagi-iiitv <anjul.ten@gmail.com>
2021-03-24 02:21:19 -0400

    Added basic tabs, separating drawing from recommendations

--- interface/src/App.jsx
+++ interface/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CanvasArea, TextInput, Gallery } from './components';
 import styles from './App.module.scss';
+import { Tabs, Tab, Panel } from '@bumaga/tabs';
 
 let urls = [
     'images/paris.jpg',
@@ -12,19 +13,70 @@ let urls = [
 class App extends React.Component {
     render() {
         return (
-            <div className={styles.App}>
-                <div className={styles.AppBody}>
-                    <div className={styles.leftContainer}>
-                        <CanvasArea />
-                    </div>
-                    {/* <div className={styles.rightContainer}>
-                        <TextInput />
-                    </div> */}
-                    <div className={styles.rightContainer}>
-                        <Gallery imageUrls={urls} />
-                    </div>
+            <Tabs>
+                <div>
+                    <Tab>
+                        <button>Draw</button>
+                    </Tab>
+                    <Tab>
+                        <button>Recommend Layouts</button>
+                    </Tab>
+                    <Tab>
+                        <button>Recommend VGs</button>
+                    </Tab>
+                    <Tab>
+                        <button>Explore Connections</button>
+                    </Tab>
                 </div>
-            </div>
+                <Panel>
+                    <div className={styles.App}>
+                        <div className={styles.AppBody}>
+                            <div className={styles.leftContainer}>
+                                <CanvasArea />
+                            </div>
+                            <div className={styles.rightContainer}>
+                                <TextInput />
+                            </div>
+                        </div>
+                    </div>
+                </Panel>
+                <Panel>
+                    <div className={styles.App}>
+                        <div className={styles.AppBody}>
+                            <div className={styles.leftContainer}>
+                                <CanvasArea />
+                            </div>
+                            <div className={styles.rightContainer}>
+                                <Gallery imageUrls={urls} />
+                            </div>
+                        </div>
+                    </div>
+                </Panel>
+                <Panel>
+                    <div className={styles.App}>
+                        <div className={styles.AppBody}>
+                            <div className={styles.leftContainer}>
+                                <CanvasArea />
+                            </div>
+                            <div className={styles.rightContainer}>
+                                <Gallery imageUrls={urls} />
+                            </div>
+                        </div>
+                    </div>
+                </Panel>
+                <Panel>
+                    <div className={styles.App}>
+                        <div className={styles.AppBody}>
+                            <div className={styles.leftContainer}>
+                                <CanvasArea />
+                            </div>
+                            <div className={styles.rightContainer}>
+                                <Gallery imageUrls={urls} />
+                            </div>
+                        </div>
+                    </div>
+                </Panel>
+            </Tabs>
         );
     }
 }
