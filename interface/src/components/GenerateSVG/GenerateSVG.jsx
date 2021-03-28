a2ece5992c27df38699a6a210424247a3ba955f3
tyagi-iiitv <anjul.ten@gmail.com>
2021-03-28 08:20:59 -0400

    Text wrapping now works

--- interface/src/components/GenerateSVG/GenerateSVG.jsx
+++ interface/src/components/GenerateSVG/GenerateSVG.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './GenerateSVG.module.scss';
 import * as d3 from 'd3';
 import { flows1 } from '../../input_flows';
+import { wrap } from './wrap';
 
 class GenerateSVG extends React.Component {
     constructor(props) {
@@ -9,7 +10,7 @@ class GenerateSVG extends React.Component {
         this.state = {
             canvasDims: { width: 1280, height: 960 },
             flow: flows1[20],
-            vg: 'svgImages/vg2.svg',
+            vg: 'svgImages/vg4.svg',
         };
     }
     componentDidMount() {
@@ -18,58 +19,76 @@ class GenerateSVG extends React.Component {
 
     render() {
         return (
-            <svg
-                id="svg"
-                className={styles.svgComp}
-                height={this.state.canvasDims.height}
-                width={this.state.canvasDims.width}
-            />
+            <div>
+                <svg
+                    id="svg"
+                    className={styles.svgComp}
+                    height={this.state.canvasDims.height}
+                    width={this.state.canvasDims.width}
+                />
+                <svg id="vg" />
+            </div>
         );
     }
 }
 
 async function generateSVG(vg, flow) {
+    // Getting the attributes of the vg
     let svg = d3.select('svg');
 
+    let divsvg = svg.node(0);
+
+    await d3.xml(vg).then((data) => {
+        divsvg.append(data.documentElement);
+    });
+
+    // rect coordinates for text wrapping
+    // let text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+    let textBoxWidth = d3.select('.text-wrap').attr('width');
+    let textBoxHeight = d3.select('.text-wrap').attr('height');
+
+    d3.select('.txt1').call(wrap, textBoxWidth);
+
     // Scale the figure with the width parameter
-    let scale = 200;
+    // let scale = 200;
 
-    // Adding VGs at certain locations on the final svg
-    for (let i = 0; i < flow.length; i++) {
-        svg.append('g')
-            .append('svg:image')
-            .attr('xlink:href', vg)
-            .attr('width', scale)
-            .attr('x', flow[i][0] - scale / 2)
-            .attr('y', flow[i][1] - scale / 2);
-    }
+    // // Adding VGs at certain locations on the final svg
+    // for (let i = 0; i < flow.length; i++) {
+    //     svg.append('g')
+    //         .append('svg:image')
+    //         .attr('xlink:href', vg)
+    //         .attr('width', scale)
+    //         .attr('x', flow[i][0] - scale / 2)
+    //         .attr('y', flow[i][1] - scale / 2);
+    // }
 
-    // Generating Lines
-    let lines = [];
-    for (let i = 0; i < flow.length - 1; i++) {
-        lines.push([flow[i][0], flow[i][1], flow[i + 1][0], flow[i + 1][1]]);
-    }
+    // // Generating Lines
+    // let lines = [];
+    // for (let i = 0; i < flow.length - 1; i++) {
+    //     lines.push([flow[i][0], flow[i][1], flow[i + 1][0], flow[i + 1][1]]);
+    // }
 
-    // DRAWING THE LINES
-    svg.append('g')
-        .selectAll('line')
-        .data(lines)
-        .enter()
-        .append('line')
-        .style('stroke', 'red')
-        .style('stroke-width', 10)
-        .attr('x1', function (d) {
-            return d[0];
-        })
-        .attr('x2', function (d) {
-            return d[2];
-        })
-        .attr('y1', function (d) {
-            return d[1];
-        })
-        .attr('y2', function (d) {
-            return d[3];
-        });
+    // // DRAWING THE LINES
+    // svg.append('g')
+    //     .selectAll('line')
+    //     .data(lines)
+    //     .enter()
+    //     .append('line')
+    //     .style('stroke', 'red')
+    //     .style('stroke-width', 10)
+    //     .attr('x1', function (d) {
+    //         return d[0];
+    //     })
+    //     .attr('x2', function (d) {
+    //         return d[2];
+    //     })
+    //     .attr('y1', function (d) {
+    //         return d[1];
+    //     })
+    //     .attr('y2', function (d) {
+    //         return d[3];
+    // });
     // console.log(svg)
 
     // svg.append('line')
