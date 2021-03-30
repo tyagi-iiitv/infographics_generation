e6e3a5ee79375fb55d2d2be95dd30bf5ade2109b
tyagi-iiitv <anjul.ten@gmail.com>
2021-03-30 01:28:53 -0400

    Added connection code for second and third use case

--- interface/src/components/GenerateSVG/GenerateSVG.jsx
+++ interface/src/components/GenerateSVG/GenerateSVG.jsx
@@ -12,9 +12,13 @@ class GenerateSVG extends React.Component {
         super(props);
         this.state = {
             canvasDims: { width: 1280, height: 960 },
-            flow: flows1[3],
-            vg: 'get-vg/vg1.svg',
+            flow: flows2[0],
+            vg: 'get-vg/vg12.svg',
+            pivot: 'images/pivot.png',
+            background: 'images/background.jpg',
             textInfo: this.props.textInfo,
+            connectionType: 'alternate', // Choose b/w {regular, pivot and alternate}
+            includeLast: true, // Whether to generate last connection or not
         };
     }
     componentDidMount() {
@@ -22,7 +26,11 @@ class GenerateSVG extends React.Component {
             this.state.vg,
             this.state.flow,
             this.state.canvasDims.width,
-            this.state.canvasDims.height
+            this.state.canvasDims.height,
+            this.state.background,
+            this.state.pivot,
+            this.state.connectionType,
+            this.state.includeLast
         );
     }
 
@@ -40,39 +48,110 @@ class GenerateSVG extends React.Component {
     }
 }
 
-async function generateSVG(vg, flow, width, height) {
+async function generateSVG(
+    vg,
+    flow,
+    width,
+    height,
+    background,
+    pivot,
+    connectionType,
+    includeLast
+) {
     let svg = d3.select('svg');
+
+    // Pivot centers for third use cases (set scale = 0.75 while loading the image for third use case)
+    // let pivotCenter = {x: width/2+100, y: 60};
+
+    // Pivot centers for second use case with scale 1
+    let pivotCenter = { x: width / 2 - 100, y: 550 };
 
     // Defining lines, angles and centers to insert connections
     let lines = [];
     let angles = [];
     let centers = [];
+    let offsetX = 200;
+    let offsetY = 100;
+
     for (let i = 0; i < flow.length - 1; i++) {
         lines.push([flow[i][0], flow[i][1], flow[i + 1][0], flow[i + 1][1]]);
-        angles.push(
-            Math.atan2(flow[i + 1][1] - flow[i][1], flow[i + 1][0] - flow[i][0]) *
-                (180 / Math.PI) +
-                180
-        );
-        centers.push([
-            (flow[i][0] + flow[i + 1][0]) / 2,
-            (flow[i][1] + flow[i + 1][1]) / 2,
-        ]);
     }
 
-    // Generating last angle and center
-    centers.push([
-        (flow[flow.length - 1][0] + flow[0][0]) / 2,
-        (flow[flow.length - 1][1] + flow[0][1]) / 2,
-    ]);
-    angles.push(
-        Math.atan2(
-            flow[0][1] - flow[flow.length - 1][1],
-            flow[0][0] - flow[flow.length - 1][0]
-        ) *
-            (180 / Math.PI) +
-            180
-    );
+    if (connectionType == 'pivot') {
+        // Generating connections b/w elements and pivot
+        for (let i = 0; i < flow.length - 1; i++) {
+            angles.push(
+                Math.atan2(flow[i][1] - pivotCenter.y, flow[i][0] - pivotCenter.x) *
+                    (180 / Math.PI) +
+                    180
+            );
+            centers.push([
+                (flow[i][0] + pivotCenter.x + offsetX) / 2,
+                (flow[i][1] + pivotCenter.y + offsetY) / 2,
+            ]);
+        }
+        // Generating the last center and angle
+        if (includeLast) {
+            centers.push([
+                (flow[flow.length - 1][0] + pivotCenter.x + offsetX) / 2,
+                (flow[flow.length - 1][1] + pivotCenter.y + offsetY) / 2,
+            ]);
+            angles.push(
+                Math.atan2(
+                    pivotCenter.y + offsetY - flow[flow.length - 1][1],
+                    pivotCenter.x + offsetX - flow[flow.length - 1][0]
+                ) *
+                    (180 / Math.PI) +
+                    180
+            );
+        }
+    } else if (connectionType == 'alternate') {
+        for (let i = 0; i < flow.length - 2; i++) {
+            angles.push(
+                Math.atan2(
+                    flow[i + 2][1] - flow[i][1],
+                    flow[i + 2][0] - flow[i][0]
+                ) *
+                    (180 / Math.PI) +
+                    180
+            );
+            centers.push([
+                (flow[i][0] + flow[i + 2][0]) / 2,
+                (flow[i][1] + flow[i + 2][1]) / 2,
+            ]);
+        }
+    } else {
+        // Generating regular connections b/w sequential elements
+        for (let i = 0; i < flow.length - 1; i++) {
+            angles.push(
+                Math.atan2(
+                    flow[i + 1][1] - flow[i][1],
+                    flow[i + 1][0] - flow[i][0]
+                ) *
+                    (180 / Math.PI) +
+                    180
+            );
+            centers.push([
+                (flow[i][0] + flow[i + 1][0]) / 2,
+                (flow[i][1] + flow[i + 1][1]) / 2,
+            ]);
+        }
+        if (includeLast) {
+            // Generating last angle and center
+            centers.push([
+                (flow[flow.length - 1][0] + flow[0][0]) / 2,
+                (flow[flow.length - 1][1] + flow[0][1]) / 2,
+            ]);
+            angles.push(
+                Math.atan2(
+                    flow[0][1] - flow[flow.length - 1][1],
+                    flow[0][0] - flow[flow.length - 1][0]
+                ) *
+                    (180 / Math.PI) +
+                    180
+            );
+        }
+    }
 
     // Insert background image
     svg.append('image')
@@ -81,25 +160,26 @@ async function generateSVG(vg, flow, width, height) {
         .attr('height', height)
         .attr('width', width)
         .attr('preserveAspectRatio', 'none')
-        .attr('href', 'images/background.jpg');
+        .attr('href', background);
 
-    let scale = 180;
+    let scaleVG = 250;
+    let scaleC = 100;
     for (let i = 0; i < flow.length; i++) {
         svg.append('g')
             .append('svg:image')
             .attr('xlink:href', vg)
-            .attr('width', scale)
-            .attr('x', flow[i][0] - scale / 2)
-            .attr('y', flow[i][1] - scale / 2);
+            .attr('width', scaleVG)
+            .attr('x', flow[i][0] - scaleVG / 2)
+            .attr('y', flow[i][1] - scaleVG / 2);
     }
 
     for (let i = 0; i < centers.length; i++) {
         svg.append('g')
             .append('svg:image')
-            .attr('xlink:href', 'connections/three-dots.svg')
-            .attr('width', scale)
-            .attr('x', centers[i][0] - scale / 2)
-            .attr('y', centers[i][1] - scale / 2)
+            .attr('xlink:href', 'connections/three-curved-arrows.svg')
+            .attr('width', scaleC)
+            .attr('x', centers[i][0] - scaleC / 2)
+            .attr('y', centers[i][1] - scaleC / 2)
             .attr('transform', function () {
                 console.log(angles[i]);
                 return (
@@ -113,6 +193,33 @@ async function generateSVG(vg, flow, width, height) {
                 );
             });
     }
+
+    svg.append('g')
+        .selectAll('line')
+        .data(lines)
+        .enter()
+        .append('line')
+        .style('stroke', 'red')
+        .style('stroke-width', 10)
+        .attr('x1', function (d) {
+            return d[0];
+        })
+        .attr('x2', function (d) {
+            return d[2];
+        })
+        .attr('y1', function (d) {
+            return d[1];
+        })
+        .attr('y2', function (d) {
+            return d[3];
+        });
+
+    // Positioning for 2nd
+    svg.append('image')
+        .attr('x', pivotCenter.x)
+        .attr('y', pivotCenter.y)
+        .attr('href', pivot);
+    // .attr('transform', 'scale(0.75)')
 
     return svg.node();
 }
