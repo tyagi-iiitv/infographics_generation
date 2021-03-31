4e6ccaf9c1d6dbbfc78e35ed44309728c9f18ff6
tyagi-iiitv <anjul.ten@gmail.com>
2021-03-30 23:01:40 -0400

    Final commit before we start generating images

--- interface/src/components/GenerateSVG/GenerateSVG.jsx
+++ interface/src/components/GenerateSVG/GenerateSVG.jsx
@@ -12,13 +12,15 @@ class GenerateSVG extends React.Component {
         super(props);
         this.state = {
             canvasDims: { width: 1280, height: 960 },
-            flow: flows1[12],
-            vg: 'svgImages/vg25.svg',
+            flow: flows2[0],
+            vg: 'svgImages/vg1.svg',
             pivot: null,
             background: 'images/background3.jpg',
             textInfo: this.props.textInfo,
             connectionType: 'regular', // Choose b/w {regular, pivot and alternate}
             includeLast: true, // Whether to generate last connection or not
+            input: input5,
+            connection: 'connections/glasses.svg',
         };
     }
     componentDidMount() {
@@ -37,7 +39,9 @@ class GenerateSVG extends React.Component {
                     this.state.background,
                     this.state.pivot,
                     this.state.connectionType,
-                    this.state.includeLast
+                    this.state.includeLast,
+                    this.state.input,
+                    this.state.connection
                 );
             });
     }
@@ -64,102 +68,104 @@ async function generateSVG(
     background,
     pivot,
     connectionType,
-    includeLast
+    includeLast,
+    input,
+    connection
 ) {
     let svg = d3.select('svg');
 
     // Pivot centers for third use cases (set scale = 0.75 while loading the image for third use case)
-    // let pivotCenter = {x: width/2+100, y: 60};
+    let pivotCenter = { x: width / 2 + 100, y: 60 };
 
     // Pivot centers for second use case with scale 1
     // let pivotCenter = { x: width / 2 - 100, y: 550 };
 
     // Defining lines, angles and centers to insert connections
-    // let lines = [];
-    // let angles = [];
-    // let centers = [];
-    // let offsetX = 200;
-    // let offsetY = 100;
+    let lines = [];
+    let angles = [];
+    let centers = [];
+    let offsetX = 200;
+    let offsetY = 100;
 
-    // for (let i = 0; i < flow.length - 1; i++) {
-    //     lines.push([flow[i][0], flow[i][1], flow[i + 1][0], flow[i + 1][1]]);
-    // }
+    for (let i = 0; i < flow.length - 1; i++) {
+        lines.push([flow[i][0], flow[i][1], flow[i + 1][0], flow[i + 1][1]]);
+    }
 
-    // if (connectionType == 'pivot') {
-    //     // Generating connections b/w elements and pivot
-    //     for (let i = 0; i < flow.length - 1; i++) {
-    //         angles.push(
-    //             Math.atan2(flow[i][1] - pivotCenter.y, flow[i][0] - pivotCenter.x) *
-    //                 (180 / Math.PI) +
-    //                 180
-    //         );
-    //         centers.push([
-    //             (flow[i][0] + pivotCenter.x + offsetX) / 2,
-    //             (flow[i][1] + pivotCenter.y + offsetY) / 2,
-    //         ]);
-    //     }
-    //     // Generating the last center and angle
-    //     if (includeLast) {
-    //         centers.push([
-    //             (flow[flow.length - 1][0] + pivotCenter.x + offsetX) / 2,
-    //             (flow[flow.length - 1][1] + pivotCenter.y + offsetY) / 2,
-    //         ]);
-    //         angles.push(
-    //             Math.atan2(
-    //                 pivotCenter.y + offsetY - flow[flow.length - 1][1],
-    //                 pivotCenter.x + offsetX - flow[flow.length - 1][0]
-    //             ) *
-    //                 (180 / Math.PI) +
-    //                 180
-    //         );
-    //     }
-    // } else if (connectionType == 'alternate') {
-    //     for (let i = 0; i < flow.length - 2; i++) {
-    //         angles.push(
-    //             Math.atan2(
-    //                 flow[i + 2][1] - flow[i][1],
-    //                 flow[i + 2][0] - flow[i][0]
-    //             ) *
-    //                 (180 / Math.PI) +
-    //                 180
-    //         );
-    //         centers.push([
-    //             (flow[i][0] + flow[i + 2][0]) / 2,
-    //             (flow[i][1] + flow[i + 2][1]) / 2,
-    //         ]);
-    //     }
-    // } else {
-    //     // Generating regular connections b/w sequential elements
-    //     for (let i = 0; i < flow.length - 1; i++) {
-    //         angles.push(
-    //             Math.atan2(
-    //                 flow[i + 1][1] - flow[i][1],
-    //                 flow[i + 1][0] - flow[i][0]
-    //             ) *
-    //                 (180 / Math.PI) +
-    //                 180
-    //         );
-    //         centers.push([
-    //             (flow[i][0] + flow[i + 1][0]) / 2,
-    //             (flow[i][1] + flow[i + 1][1]) / 2,
-    //         ]);
-    //     }
-    //     if (includeLast) {
-    //         // Generating last angle and center
-    //         centers.push([
-    //             (flow[flow.length - 1][0] + flow[0][0]) / 2,
-    //             (flow[flow.length - 1][1] + flow[0][1]) / 2,
-    //         ]);
-    //         angles.push(
-    //             Math.atan2(
-    //                 flow[0][1] - flow[flow.length - 1][1],
-    //                 flow[0][0] - flow[flow.length - 1][0]
-    //             ) *
-    //                 (180 / Math.PI) +
-    //                 180
-    //         );
-    //     }
-    // }
+    if (connectionType == 'pivot') {
+        // Generating connections b/w elements and pivot
+        for (let i = 0; i < flow.length - 1; i++) {
+            angles.push(
+                Math.atan2(flow[i][1] - pivotCenter.y, flow[i][0] - pivotCenter.x) *
+                    (180 / Math.PI) +
+                    180
+            );
+            centers.push([
+                (flow[i][0] + pivotCenter.x + offsetX) / 2,
+                (flow[i][1] + pivotCenter.y + offsetY) / 2,
+            ]);
+        }
+        // Generating the last center and angle
+        if (includeLast) {
+            centers.push([
+                (flow[flow.length - 1][0] + pivotCenter.x + offsetX) / 2,
+                (flow[flow.length - 1][1] + pivotCenter.y + offsetY) / 2,
+            ]);
+            angles.push(
+                Math.atan2(
+                    pivotCenter.y + offsetY - flow[flow.length - 1][1],
+                    pivotCenter.x + offsetX - flow[flow.length - 1][0]
+                ) *
+                    (180 / Math.PI) +
+                    180
+            );
+        }
+    } else if (connectionType == 'alternate') {
+        for (let i = 0; i < flow.length - 2; i++) {
+            angles.push(
+                Math.atan2(
+                    flow[i + 2][1] - flow[i][1],
+                    flow[i + 2][0] - flow[i][0]
+                ) *
+                    (180 / Math.PI) +
+                    180
+            );
+            centers.push([
+                (flow[i][0] + flow[i + 2][0]) / 2,
+                (flow[i][1] + flow[i + 2][1]) / 2,
+            ]);
+        }
+    } else {
+        // Generating regular connections b/w sequential elements
+        for (let i = 0; i < flow.length - 1; i++) {
+            angles.push(
+                Math.atan2(
+                    flow[i + 1][1] - flow[i][1],
+                    flow[i + 1][0] - flow[i][0]
+                ) *
+                    (180 / Math.PI) +
+                    180
+            );
+            centers.push([
+                (flow[i][0] + flow[i + 1][0]) / 2,
+                (flow[i][1] + flow[i + 1][1]) / 2,
+            ]);
+        }
+        if (includeLast) {
+            // Generating last angle and center
+            centers.push([
+                (flow[flow.length - 1][0] + flow[0][0]) / 2,
+                (flow[flow.length - 1][1] + flow[0][1]) / 2,
+            ]);
+            angles.push(
+                Math.atan2(
+                    flow[0][1] - flow[flow.length - 1][1],
+                    flow[0][0] - flow[flow.length - 1][0]
+                ) *
+                    (180 / Math.PI) +
+                    180
+            );
+        }
+    }
 
     // Insert background image
     svg.append('image')
@@ -169,12 +175,6 @@ async function generateSVG(
         .attr('width', width)
         .attr('preserveAspectRatio', 'none')
         .attr('href', background);
-
-    // Generating Lines
-    let lines = [];
-    for (let i = 0; i < flow.length - 1; i++) {
-        lines.push([flow[i][0], flow[i][1], flow[i + 1][0], flow[i + 1][1]]);
-    }
 
     let scale = 350;
     for (let i = 0; i < flow.length; i++) {
@@ -193,16 +193,16 @@ async function generateSVG(
         // Insert the new text from text field
         svg.select(`#vg${i}`)
             .select('.txt1')
-            .text(input4[i].text)
+            .text(input[i].text)
             .call(wrap, textBoxWidth);
 
         // Insert Label
-        svg.select(`#vg${i}`).select('.lb1').text(input4[i].label);
+        svg.select(`#vg${i}`).select('.lb1').text(input[i].label);
 
         // Should be the same for title, but I was unable to find the class for it
 
         // Insert image
-        svg.select(`#vg${i}`).select('.img1').attr('href', input4[i].image);
+        svg.select(`#vg${i}`).select('.img1').attr('href', input[i].image);
 
         // Change class for each svg element
         svg.selectAll('.color-1')
@@ -215,11 +215,11 @@ async function generateSVG(
         let orgStyle = svg.select(`#vg${i}`).select('style').text();
         svg.select(`#vg${i}`)
             .select('style')
-            .text(orgStyle + input4[i].color);
+            .text(orgStyle + input[i].color);
     }
 
     // let scaleVG = 250;
-    // let scaleC = 100;
+    let scaleC = 100;
     // for (let i = 0; i < flow.length; i++) {
     //     svg.append('g')
     //         .append('svg:image')
@@ -229,26 +229,26 @@ async function generateSVG(
     //         .attr('y', flow[i][1] - scaleVG / 2);
     // }
 
-    // for (let i = 0; i < centers.length; i++) {
-    //     svg.append('g')
-    //         .append('svg:image')
-    //         .attr('xlink:href', 'connections/arrow3.svg')
-    //         .attr('width', scaleC)
-    //         .attr('x', centers[i][0] - scaleC / 2)
-    //         .attr('y', centers[i][1] - scaleC / 2)
-    //         .attr('transform', function () {
-    //             console.log(angles[i]);
-    //             return (
-    //                 'rotate(' +
-    //                 angles[i] +
-    //                 ',' +
-    //                 centers[i][0] +
-    //                 ',' +
-    //                 centers[i][1] +
-    //                 ')'
-    //             );
-    //         });
-    // }
+    for (let i = 0; i < centers.length; i++) {
+        svg.append('g')
+            .append('svg:image')
+            .attr('xlink:href', connection)
+            .attr('width', scaleC)
+            .attr('x', centers[i][0] - scaleC / 2)
+            .attr('y', centers[i][1] - scaleC / 2)
+            .attr('transform', function () {
+                console.log(angles[i]);
+                return (
+                    'rotate(' +
+                    angles[i] +
+                    ',' +
+                    centers[i][0] +
+                    ',' +
+                    centers[i][1] +
+                    ')'
+                );
+            });
+    }
 
     // svg.append('g')
     //     .selectAll('line')
--- interface/src/components/GenerateSVG/sampleInput.js
+++ interface/src/components/GenerateSVG/sampleInput.js
@@ -39,45 +39,49 @@ let input4 = [
 
 let input5 = [
     {
-        label: 'Los Angeles',
+        label: 'USA',
         title: '1',
         text:
-            ' The entire world population could fit inside LA, if everyone stands shoulder to shoulder',
+            ' The United States has maintained its position of being the world’s largest economy since 1871.',
         imageAlt: '',
-        image: 'images/la.jpg',
-        color: '.color1{fill:#8dd3c7;}',
+        image: 'images/usa.svg',
+        color: '.color-10{fill:#1b9e77;}',
     },
     {
-        label: 'France',
-        title: '2',
-        text: ' More people visit France than any other country',
-        imageAlt: '',
-        image: 'images/paris.jpg',
-        color: '.color1{fill:#ffffb3;}',
-    },
-    {
-        label: 'Canary Islands',
-        title: '3',
-        text: ' Canary Islands are named after dogs',
-        imageAlt: '',
-        image: 'images/canary_islands.jpg',
-        color: '.color1{fill:#bebada;}',
-    },
-    {
-        label: 'Indonesia',
+        label: 'CHINA',
         title: '4',
-        text: " Indonesia has some of the world's shortest people",
+        text:
+            ' In China, the dragons are associated with wealth, power, and leadership',
         imageAlt: '',
-        image: 'images/indonesia.jpg',
-        color: '.color1{fill:#fb8072;}',
+        image: 'images/china.svg',
+        color: '.color-13{fill:#d95f02;}',
     },
     {
-        label: 'Nepal',
-        title: '5',
-        text: ' Mount Everest is bigger than the last time it was measured',
+        label: 'INDIA',
+        title: '2',
+        text:
+            ' It’s estimated that between 15% and 30% of India’s population follow a strict vegetarian diet.',
         imageAlt: '',
-        image: 'images/nepal.jpg',
-        color: '.color1{fill:#80b1d3;}',
+        image: 'images/india.svg',
+        color: '.color-11{fill:#7570b3;}',
+    },
+    {
+        label: 'RUSSIA',
+        title: '3',
+        text:
+            ' Russia is 60% forest. What’s more interesting, half of that forest is totally uninhabited by humans.',
+        imageAlt: '',
+        image: 'images/russia.svg',
+        color: '.color-12{fill:#e7298a;}',
+    },
+    {
+        label: 'BRAZIL',
+        title: '5',
+        text:
+            ' Brazil has won more football world cups than any other country, with Pele being the top scorer.',
+        imageAlt: '',
+        image: 'images/brazil.svg',
+        color: '.color-14{fill:#66a61e;}',
     },
 ];
 
